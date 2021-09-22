49d9376c0346a668a50184bf3377d1d60f7398cf
Weverton <wevertoncesar@gmail.com>
2021-09-22 09:37:24 -0300

    Refactor student test

--- PaymentContext.Test/Entities/StudentTests.cs
+++ PaymentContext.Test/Entities/StudentTests.cs
@@ -1,5 +1,7 @@
 using System;
 using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
 using Xunit;
 
 namespace PaymentContext.Test.Entities
@@ -11,7 +13,10 @@ namespace PaymentContext.Test.Entities
         {
             var expireDate = new DateTime(31, 12, 2021);
             var subscription = new Subscription(expireDate);
-            var student = new Student("Weverton", "Paulino", "12345678900", "wevertoncesar@gmail.com");
+            var name = new Name("Weverton", "Paulino");
+            var document = new Document("12345678900", EDocumentType.CPF);
+            var email = new Email("wevertoncesar@gmail.com");
+            var student = new Student(name, document, email);
             student.AddSubscription(subscription);
         }
     }
